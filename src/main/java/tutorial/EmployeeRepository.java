c45cbbfa1ff191cb2ada5d339a3b40058200df39
Hivison do Nascimento Moura <hivison.moura@gmail.com>
2016-12-26 17:48:37 -0200

    Created some classes for undestand the behavior of the Spring Boot with ReactJS

--- src/main/java/tutorial/EmployeeRepository.java
+++ src/main/java/tutorial/EmployeeRepository.java
@@ -7,9 +7,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 /**
  * Created by hivisonmoura on 2016-12-23.
  */
-public class EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+
     @Override
     void delete(Long aLong);
 }
